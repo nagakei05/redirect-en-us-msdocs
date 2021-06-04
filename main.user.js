514623a454d9f13f10f562f2849c32c8d5fa236b
Shohei Morinaga <nagakei05@gmail.com>
2021-06-04 14:22:33 +0900

    changed script to toggle mode

--- main.user.js
+++ main.user.js
@@ -6,30 +6,30 @@
 // @author       shnarita
 // @match        https://docs.microsoft.com/*
 // @grant        none
-// @updateURL    https://github.com/shnrr/redirect-en-us-msdocs/raw/master/main.user.js
-// @downloadURL  https://github.com/shnrr/redirect-en-us-msdocs/raw/master/main.user.js
-// @supportURL   https://github.com/shnrr/redirect-en-us-msdocs/
+// @updateURL    https://github.com/nagakei05/redirect-en-us-msdocs/raw/master/main.user.js
+// @downloadURL  https://github.com/nagakei05/redirect-en-us-msdocs/raw/master/main.user.js
+// @supportURL   https://github.com/nagakei05/redirect-en-us-msdocs/
 // ==/UserScript==
 
 (function () {
 
+  const msDocsUrlReg = new RegExp('https://docs.microsoft.com/\\w{2}-\\w{2}/(.*)', 'ig');
+  const msDocsJAJPUrlReg = new RegExp('https://docs.microsoft.com/ja-jp/(.*)', 'ig');
+  const msDocsENUSUrlReg = new RegExp('https://docs.microsoft.com/en-us/(.*)', 'ig');
+
   document.onkeydown = function (e) {
-    if (e.keyCode == 39) {
-      // 右カーソル キーをクリックで en-us に切り替え
+    if (e.keyCode == 220) {
+      // "\" キーを押下で en-us/ja-jp 間を切り替え
       const currentUrl = location.href; // 現在のページのURLを取得
-      const msDocsUrlReg = new RegExp('https://docs.microsoft.com/\\w{2}-\\w{2}/(.*)', 'ig');
+      const nextpage = currentUrl; // nextpage 初期化
       if (currentUrl.match(msDocsUrlReg)) {
-        const nextPage = currentUrl.replace(msDocsUrlReg, "https://docs.microsoft.com/en-us/$1"); // マッチさせた文字列以外を置換
-        console.log({"nextPage" : nextPage});
-        location.href = nextPage;
-      }
-    }
-    if (e.keyCode == 37) {
-      // 左カーソル キーをクリックで ja-jp に切り替え
-      const currentUrl = location.href; // 現在のページのURLを取得
-      const msDocsUrlReg = new RegExp('https://docs.microsoft.com/\\w{2}-\\w{2}/(.*)', 'ig');
-      if (currentUrl.match(msDocsUrlReg)) {
-        const nextPage = currentUrl.replace(msDocsUrlReg, "https://docs.microsoft.com/ja-jp/$1"); // マッチさせた文字列以外を置換
+        if (currentUrl.match(msDocsJAJPUrlReg)) {
+          nextPage = currentUrl.replace(msDocsUrlReg, "https://docs.microsoft.com/en-us/$1"); // マッチさせた文字列以外を置換
+        } else if (currentUrl.match(msDocsENUSUrlReg)) {
+          nextPage = currentUrl.replace(msDocsUrlReg, "https://docs.microsoft.com/ja-jp/$1"); // マッチさせた文字列以外を置換
+        } else {
+          nextPage = currentUrl.replace(msDocsUrlReg, "https://docs.microsoft.com/en-us/$1"); // マッチさせた文字列以外を置換
+        }
         console.log({"nextPage" : nextPage});
         location.href = nextPage;
       }
